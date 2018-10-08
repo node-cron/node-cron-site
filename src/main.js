780eabb2bbc3e4864d2271395d2645be069f126a
merencia <lucas.merencia@gmail.com>
2018-10-08 17:27:49 -0300

    added usage

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import VueRouter from 'vue-router';
 import Home from './pages/home.vue';
 import Docs from './pages/docs.vue';
 
-import GettingStarted from './pages/docs-pages/getting-started.vue';
-import CronSyntax from './pages/docs-pages/cron-syntax.vue';
-
 import './styles/base.scss';
 import './img/favicon-16x16.png';
 import './img/favicon-32x32.png';
@@ -16,25 +13,7 @@ const routes = [
     { path: '/', component: Home },
     { 
         path: '/docs', 
-        component: Docs,
-        children: [
-            { 
-                path: '/',
-                redirect: '/docs/getting-started'
-            },
-            { 
-                path: '/docs/getting-started',
-                components:{ 
-                    docview: GettingStarted 
-                }
-            },
-            { 
-                path: '/docs/cron-syntax',
-                components:{ 
-                    docview: CronSyntax 
-                }
-            }
-        ]
+        component: Docs
     }
 ];
 
--- src/pages/docs.vue
+++ src/pages/docs.vue
@@ -1,11 +1,15 @@
 <template>
     <page-layout>
-        <container class="docs-container">
-            <div class="docs-menu-container">
-                <docs-menu></docs-menu>
-            </div>
-            <div class="docs-content-container">
-                <router-view name="docview"></router-view>
+        <container>
+            <div>
+                <h1>Documentation</h1>
+                <p>
+                    The node-cron module is tiny task scheduler in pure JavaScript for node.js 
+                    based on GNU crontab. This module allows you to schedule task in node.js using 
+                    full crontab syntax.
+                </p>
+                <getting-started></getting-started>
+                <cron-syntax></cron-syntax>
             </div>  
         </container>
     </page-layout>
@@ -14,13 +18,18 @@
 <script>
 import PageLayout from '../layouts/page-layout.vue';
 import Container from '../components/container.vue';
-import DocsMenu from '../components/docs-menu.vue';
+
+
+import GettingStarted from './docs-pages/getting-started.vue';
+import CronSyntax from './docs-pages/cron-syntax.vue';
+
 
 export default {
   components: {
     PageLayout,
     Container,
-    DocsMenu
+    GettingStarted,
+    CronSyntax
   }
 }
 </script>
